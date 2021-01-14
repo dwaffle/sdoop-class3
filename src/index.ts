331942295ad727e7af3b5798be5400570b3a7d48
Doug Waffle <dwaffle13154@alumni.focuscollege.com>
2021-01-13 18:23:04 -0800

    Added Remote, Room, Signal and Comments

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import prompt from 'prompt';
+import {Room} from './classes/class.room'
+import {Remote} from './classes/class.remote'
+import { release } from 'os';
 
+const remote = new Remote();
+Room.items.push(remote);
 prompt.start();
 
 function listenForButtonInput(){
@@ -10,7 +15,13 @@ function listenForButtonInput(){
         required: true
     }], (err:any, result:any) => {
         if(!err){
-            console.log( result.button );
+            //This is where we start interacting with the Remote.
+            const touchedButton = remote.buttons.find(button => button.emblem === result.button)
+            if(touchedButton.isPressed){
+                touchedButton.release()
+            } else {
+                touchedButton.press()
+            }
             listenForButtonInput();
         }
     })
