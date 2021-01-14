06b85ab143f1de1aa52bdd824b6ee55d23485a41
Bradley Holbrook <bradley@gamesheetinc.com>
2021-01-13 17:03:37 -0800

    Prompt for Button Input

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("SDOOP Class 3")
+import prompt from 'prompt';
+
+prompt.start();
+
+function listenForButtonInput(){
+    prompt.get([{
+        name: 'button',
+        description: 'Press or Release Button',
+        type: 'string',
+        required: true
+    }], (err:any, result:any) => {
+        if(!err){
+            console.log( result.button );
+            listenForButtonInput();
+        }
+    })
+}
+
+listenForButtonInput();
